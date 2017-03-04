540657ec1c4fdb1da8016116d1c229ee743b4fa0
Nik Patel <nikspatel007@gmail.com>
2017-03-04 14:03:42 -0600

    adding userApi

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,31 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  //Hard coding for simplicity. Pretend this hits a realdatabase.
+  res.json([{
+      "id": 1,
+      "firstName": "Nik",
+      "lastName": "Patel",
+      "email": "nikspatel007@gmail.com"
+    },
+    {
+      "id": 2,
+      "firstName": "John",
+      "lastName": "Smith",
+      "email": "johnsmith@test.com"
+    },
+    {
+      "id": 3,
+      "firstName": "Jane",
+      "lastName": "Doe",
+      "email": "janedoe@test.com"
+    },
+  ])
+})
+
+
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import './index.css';
+import {
+  getUsers
+} from './api/userApi';
 
-import numeral from 'numeral';
+getUsers().then(result => {
+  let userBody = "";
+  result.forEach(user => {
+    userBody += `<tr>
+        <td><a href="# data-id="${user.id}" class="deleteUser"> Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`
+  });
 
-const value = numeral(100).format('$0,0.00');
-console.log(`It will only cost you ${value}. `);
+  global.document.getElementById('users').innerHTML = userBody;
+
+});
